562eca72d509d7ffe661d540032c891b1ee020fa
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2024-05-15 22:38:52 +0300

    colorscheme (gruvbox)

--- init.lua
+++ init.lua
@@ -884,12 +884,13 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
+  --[[ { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -903,7 +904,24 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
     end,
+  }, ]]
+  {
+    -- 'catppuccin/nvim', init = function() vim.cmd.colorscheme 'catppuccin' end,
+    -- 'bluz71/vim-moonfly-colors', init = function() vim.cmd.colorscheme 'moonfly' end,
+    --[[   { 'Mofiqul/dracula.nvim' }, ]]
+    --[[   { 'rebelot/kanagawa.nvim' }, ]]
+    -- 'morhetz/gruvbox', init = function() vim.g.gruvbox_contrast_dark = 'hard'; vim.cmd.colorscheme 'gruvbox' end,
+    'morhetz/gruvbox',
+    init = function()
+      vim.g.gruvbox_contrast_dark = 'hard'
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+
+    priority = 1000,
   },
 
   -- rainbow brackets
