6b625623b323187255506d692c3f9457808b9343
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2024-05-16 10:54:25 +0300

    gruvbox-material: add foreground setting

--- init.lua
+++ init.lua
@@ -937,6 +937,7 @@ require('lazy').setup({
     lazy = false,
     config = function()
       vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_foreground = 'mix'
       vim.g.gruvbox_material_enable_italic = true
       vim.cmd.colorscheme 'gruvbox-material'
     end,
