a27c8e5d25b3bdd4ea1e9787ec4258be3f34612d
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2024-05-21 09:42:46 +0300

    scrollbar

--- init.lua
+++ init.lua
@@ -1022,7 +1022,12 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter-context',
   },
 
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+  { -- adds a scrollbar
+    'petertriho/nvim-scrollbar',
+    opts = {},
+  },
+
+  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
