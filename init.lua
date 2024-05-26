49c4c5c8a2cfad678e2780339e27b966171ab67f
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2024-05-26 12:59:59 +0300

    add vim-fugitive

--- init.lua
+++ init.lua
@@ -1027,6 +1027,10 @@ require('lazy').setup({
     opts = {},
   },
 
+  { -- better git integration
+    'tpope/vim-fugitive',
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
